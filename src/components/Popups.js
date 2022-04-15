f63fdd6f18a959bacc0fbfe1908d43d0f0909d76
Losarp <losarpeg [at] yahoo.com>
2022-04-15 23:09:03 +0300

    Style mint popup

--- src/components/Popups.js
+++ src/components/Popups.js
@@ -6,7 +6,7 @@ const Popups = (props) => {
                 <div className="popup-width">
                     <div className="popup">
                         <h2>Mint Successful!</h2> <a className="close" href="#" onClick={() => props.toggleMintVisible()}>×</a>
-                        <div className="content"> Congratulations,you’ve successfully minted an NFT!</div>
+                        <div className="content mintbox-content"> Congratulations! You’ve successfully minted an NFT!</div>
                         <div className="popupBtn"><a className="bark" href="/mycollection">See my NFT</a> <a className="light"
                                                                                                  href="#">Mint
                             Again</a></div>

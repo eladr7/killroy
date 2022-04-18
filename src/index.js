c2f1897aef0f06888c636eb26e301dc1c5870b77
Losarp <losarpeg [at] yahoo.com>
2022-04-18 17:12:10 +0300

    Root router-dom

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={'/gyld-react'}>
+    <BrowserRouter basename={'/'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
@@ -18,3 +18,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+

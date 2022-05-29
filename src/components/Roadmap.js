0fd14ea72334b7873ffbbbf5d77bc359d055f202
elad <elad@scrtlabs.com>
2022-05-29 14:44:13 +0300

    Restyled roadmap

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -7,81 +7,52 @@ const Roadmap = () => {
       <div className="background-zig-zag">
         <div className="main-content">
           <div className="title">
-            <h3> 20% Stage 1</h3>{" "}
+            <h3>25% Stage 1</h3>{" "}
           </div>
           <div className="content-text">
-            {/* <h3>The Council of Gyld</h3> */}
+            <h3>The Premiere Drop</h3>
+            <p>The KillRoy Was Here Premiere NFT Bundle is made available to</p>
             <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt.
+              the public and to whitelist/Council of Gyld holders on June 2nd,
+              2022.
             </p>
           </div>
         </div>
         <div className="main-content-two">
           <div className="title-two">
-            <h3> 40% Stage 2</h3>{" "}
+            <h3>50% Stage 2</h3>{" "}
           </div>
           <div className="content-text-two">
-            {/* <h3>Whitelist</h3> */}
+            <h3>The KillRoy Live Screening</h3>
             <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt.
+              Collectors in Austin enjoy an in-person screening of KillRoy Was
+              Here with Kevin Smith, as well as an intimate Q&A with him
+              afterwards and a wild afterparty with Kevin and the Secret Labs
+              team.
             </p>
           </div>
         </div>
         <div className="main-content-three">
           <div className="title-three">
-            <h3>60% Stage 3</h3>{" "}
+            <h3>75% Stage 3</h3>{" "}
           </div>
           <div className="content-text-three">
-            {/* <h3>Staking Bonus</h3> */}
-            <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt.
-            </p>
+            <h3>The KillRoy Was Here NFT Drop on Legendao</h3>
+            <p>KillRoy Was Here drops on the legendao.io Mint Lab.</p>
           </div>
         </div>
         <div className="main-content-four">
           <div className="title-four">
-            <h3>60% Stage 4</h3>{" "}
+            <h3>100% Stage 4</h3>{" "}
           </div>
           <div className="content-text-four">
-            {/* <h3>Meet Your 3D Avatar</h3> */}
+            <h3>The Legendary NFTs Drop</h3>
             <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt.
-            </p>
-          </div>
-        </div>
-        <div className="main-content-five">
-          <div className="title-five">
-            <h3>100% Stage 5</h3>{" "}
-          </div>
-          <div className="content-text-five">
-            {/* <h3>Gain a Free Lootbox</h3> */}
-            <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt.
+              The final evolution of the KillRoy PFP. Burning four KillRoy Was
+              Here PFPs guarantees a Legendary Golden KillRoy, which will reward
+              a significant amount of $LGND. This will grant a collector a
+              certain amount of $LGND as a free airdrop. The higher the rank is
+              of the Golden KillRoy you receive, the higher the reward.
             </p>
           </div>
         </div>

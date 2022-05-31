7715edb1e87db4b5699a28a8c5f449e264a0d76f
elad <elad@scrtlabs.com>
2022-05-31 18:16:49 +0300

    Adjusted to large screens

--- src/components/secureValueToggler.js
+++ src/components/secureValueToggler.js
@@ -4,22 +4,22 @@ function SecureValueToggler(props) {
   const [show, setShow] = useState(false);
 
   return (
-    <div className="passphrase-box">
-      <div className="lock-image-wrapper">
-        {!show && (
-          <img
-            className="passphrase-lock"
-            src="./image/lock.png"
-            alt={""}
-            onClick={() => {
-              setShow(!show);
-            }}
-          />
-        )}
-      </div>
+    <div className="container">
+      {!show && (
+        <div
+          className="abs-box stack-top"
+          onClick={() => {
+            setShow(!show);
+          }}
+        >
+          <img className="passphrase-lock" src="./image/lock.png" alt={""} />
+        </div>
+      )}
+
+      {/* <div className="box" style={{ background: "red" }}></div> */}
       <button
-        style={{ filter: `${!show ? "blur(5px)" : ""}` }}
-        className="passphrase-text"
+        style={{ filter: `${!show ? "blur(7px)" : ""}` }}
+        className="abs-box passphrase-text2"
         onClick={() => {
           setShow(!show);
         }}
@@ -31,3 +31,28 @@ function SecureValueToggler(props) {
 }
 
 export default SecureValueToggler;
+{
+  /* <div className="passphrase-box">
+<div className="lock-image-wrapper">
+  {!show && (
+    <img
+      className="passphrase-lock"
+      src="./image/lock.png"
+      alt={""}
+      onClick={() => {
+        setShow(!show);
+      }}
+    />
+  )}
+</div>
+<button
+  style={{ filter: `${!show ? "blur(5px)" : ""}` }}
+  className="passphrase-text"
+  onClick={() => {
+    setShow(!show);
+  }}
+>
+  {props.secureValue}
+</button>
+</div> */
+}
